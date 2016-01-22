775238e05693ebb3d55ffb5913d5e9b36e78cdcb
Leonardo Robaskievicz <leonardo@tecworks.com.br>
2016-01-22 13:20:51 -0200

    Corrigido classe model sgbd

--- application/models/sgbd.php
+++ application/models/sgbd.php
@@ -24,14 +24,20 @@ class SGBD extends CI_Model
 	/* 
 		FUNÇÃO PARA SETAR TODOS OS DADOS DA CONEXÃO PASSADAS POR PARAMETRO
 		NO MOMENTO DA CRIAÇÃO DO OBJETO
+
+		@param void 
+		@return void
 	*/
 	public function __construct() 
 	{
-		parent::__contruct();
+		parent::__construct();
 	}
 	/*
 		FUNÇÃO PARA DESTRUIR TODOS OS DADOS DA CONEÇÃO QUANDO O SCRIPT
 		TERMINAR DE SER EXECUTADO
+
+		@param void 
+		@return void
 	*/
 	public function __destruct () 
 	{
@@ -41,7 +47,12 @@ class SGBD extends CI_Model
 		$this->banco = "";
 		$this->link  = NULL;
 	}
-	/* FUNÇÃO PARA CRIAR CONEXÃO COM O BANCO DE DADOS */
+	/* 
+		FUNÇÃO PARA CRIAR CONEXÃO COM O BANCO DE DADOS 
+
+		@param { string host, string user, string senha, string banco }
+		@return true -> deu certo ou false -> não foi possível conectar
+	*/
 	public function criaConexao ($host, $user, $senha, $banco) 
 	{
 		// Atribui as variaveis 
@@ -62,20 +73,29 @@ class SGBD extends CI_Model
 	}
 	/*
 		Função para criar uma conex~~ao persistente com o banco de dados 
+
+		@param { string host, string user, string senha, string banco }
+		@return true -> deu certo ou false -> não foi possível conectar
 	*/
-	public function criaConexaoPersistente () ,
+	public function criaConexaoPersistente ($host, $user, $senha, $banco)
 	{
 		try {
-			$this->link = new PDO('mysql:host='.$this->host.';dbname='.$this->banco.';charset=utf8', $this->user, $this->pass, array(PDO::ATTR_PERSISTENT => true));
+			$this->load->database($banco);
+			/*$this->link = new PDO('mysql:host='.$this->host.';dbname='.$this->banco.';charset=utf8', $this->user, $this->pass, array(PDO::ATTR_PERSISTENT => true));
 		    $this->link->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 		    $this->link->setAttribute(PDO::ATTR_CASE, PDO::CASE_LOWER);
-		    $this->link->setAttribute(PDO::ATTR_TIMEOUT,300);
+		    $this->link->setAttribute(PDO::ATTR_TIMEOUT,300);*/
 		    return True;
 		} catch (PDOException $e) {
 			return False;
 		}
 	}
-	/* FUNÇÃO PARA FAZER BUSCAS NO BANCO DE DADOS DE ACORDO COM O SQL PASSADO NO PARAMETRO */
+	/* 
+		FUNÇÃO PARA FAZER BUSCAS NO BANCO DE DADOS DE ACORDO COM O SQL PASSADO NO PARAMETRO 
+
+		@param string query -> query SQL para realizar o SELECT
+		@return false -> se deu errado ou object -> com resultador do banco de dados
+	*/
 	public function busca ($sql) 
 	{
 		try {
@@ -87,54 +107,74 @@ class SGBD extends CI_Model
 			return FALSE;
 		}
 	}
-	/* FUNÇÃO PARA INCLUIR DADOS NO BANCO DE DADOS VIA SQL */
-	public function insere ($tabela, $colunas, $valores) {
+	/* 
+		FUNÇÃO PARA INCLUIR DADOS NO BANCO DE DADOS VIA SQL 
+
+		@param {string tabela, array $dados}
+		@return bool
+	*/
+	public function insere ($tabela, $dados) 
+	{
 		try {
-			// Corrige a quantidade de parametros genericos ?
-			$valoresParam = "";
-			for ($i = 0; $i < count($valores); $i++) { $i == (count($valores) - 1) ? $valoresParam .= "?" : $valoresParam .= "?, "; }
-			// Cria SQL de acordo com o que foi passado
-			$insere = $this->link->prepare("INSERT IGNORE INTO ".$tabela." (".$colunas.") VALUES (".$valoresParam.")");
-			// Vincula as colunas com os valores passados
-			for ($i = 0; $i < count($valores); $i++) { $insere->bindParam(($i+1), $valores[$i]); }
-			// Executa SQL preparada e retorna
-			return ($insere->execute());
+			$query = $this->db->insert_string($tabela, $dados);// Trata string SQL passado como parametro
+			$insere = $this->db->simple_query($query);
+			return true;
 		} catch (PDOException $e) {
 			$this->errorLog ("Erro ao inserir dados no banco de dados. PDO Error: {$e->getMessage()}\n");
-			return FALSE;
+			return false;
 		}
 	}
-	/* FUNÇÃO PARA ALTERAR OS DADOS DE UMA TABELA NO BANCO DE DADOS VIA SQL */
-	function altera ($sql) {
+	/* 
+		FUNÇÃO PARA ALTERAR OS DADOS DE UMA TABELA NO BANCO DE DADOS VIA SQL 
+		
+		@param string $sql
+		@return bool
+	*/
+	public function altera ($sql) 
+	{
 		try {
-			$altera = $this->link->exec($sql);
+			$altera = $this->db->simple_query($sql);
 			return ($altera);
 		} catch (PDOException $e) {
 			$this->errorLog ("Erro ao alterar dados no banco de dados. PDO Error: {$e->getMessage()}\n");
-			return FALSE;
+			return false;
 		}
 	}
-	/* FUNÇÃO PARA APAGAR OS DADOS DE UMA TABELA DO BANCO VIA SQL QUERY */
-	function apaga ($sql) {
+	/* 
+		FUNÇÃO PARA APAGAR OS DADOS DE UMA TABELA DO BANCO VIA SQL QUERY 
+
+		@param string $sql
+		@return bool
+	*/
+	public function apaga ($sql) 
+	{
 		try {
-			$apaga = $this->link->exec($sql);
+			$apaga = $this->db->simple_query($sql);
 			return ($apaga);
 		} catch (PDOException $e) {
 			$this->errorLog ("Erro ao apagar dados no banco de dados. PDO Error: {$e->getMessage()}\n");
 			return FALSE;
 		}
 	}
-	/* FUNCAO PARA LIMPAR TODA A TABELA DO BANCO DE DADOS */
-	function limpaTabela ($sql) {
+	/* 
+		FUNCAO PARA LIMPAR TODA A TABELA DO BANCO DE DADOS 
+		
+		@param string $tabela
+		@return bool
+	*/
+	public function limpaTabela ($tabela) 
+	{
 		try {
-			$limpaTabela = $this->link->exec($sql);
+			$limpaTabela = $this->db->simple_query("TRUNCATE TABLE ".$tabela."");
 			return ($limpaTabela);
 		} catch (PDOException $e) {
 			$this->errorLog ("Erro ao limpar tabela do banco de dados. PDO Error: {$e->getMessage()}\n");
 			return FALSE;
 		}
 	}
-	/* FUNCAO PARA BUSCA DADOS BLOB NO BANCO DE DADOS COMO IMAGENS E BULA */
+	/* 
+		FUNCAO PARA BUSCA DADOS BLOB NO BANCO DE DADOS COMO IMAGENS E BULA 
+
 	function buscaBlob ($sql) {
 		try {
 			$busca = $this->link->prepare($sql);
@@ -146,10 +186,12 @@ class SGBD extends CI_Model
 			$this->errorLog ("Erro ao buscar dados no banco de dados. PDO Error: {$e->getMessage()}\n");
 			return FALSE;
 		}
-	}
-	/* Funcao que e executada quando algum comando SQL falhar, entao e salvo um arquivo chamado errorLog.txt */
+	}*/
+	/* 
+		Funcao que e executada quando algum comando SQL falhar, entao e salvo um arquivo chamado errorLog.txt 
+	*/
 	private function errorLog ($erro) {
-		$arq = fopen($_SERVER['DOCUMENT_ROOT']."/ErrorLog/errorLog.txt","a+");
+		$arq = fopen(base_url()."errorlog/errorLog.txt","a+");
 		if ($arq != FALSE) {
 			fwrite($arq, "\n======================================\n");
 			fwrite($arq, "Data: ".date("d/m/Y as h:i:s")."\n");
