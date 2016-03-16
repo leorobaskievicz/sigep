949604bb7a6531edf2d58ee81f5b61f015d76c2e
Leonardo Robaskievicz <leonardo@tecworks.com.br>
2016-03-15 21:45:35 -0300

    Corrigido nome do driver pdo

--- application/config/database.php
+++ application/config/database.php
@@ -79,7 +79,7 @@ $db['default'] = array(
 	'username' => 'tecworks',
 	'password' => 'auj267',
 	'database' => 'ecommerce',
-	'dbdriver' => 'PDO',
+	'dbdriver' => 'pdo',
 	'dbprefix' => '',
 	'pconnect' => FALSE,
 	'db_debug' => (ENVIRONMENT !== 'production'),
